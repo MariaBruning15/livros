92384d6d1cbe2a159fd6a02731c447e7ebff0795
MariaBruning15 <mariaeduardabruning@gmail.com>
2023-08-24 20:01:08 -0300

    index e navbar

--- index.php
+++ index.php
@@ -10,32 +10,36 @@
 
 <body>
 
-    <div class="div-logo">
-        <img class="logo" src="img/logo.png">
-        <div class="tools">
-            <div class="cadastro">
-                <a href="cadastro.php" class="cadastro-link"> Cadastro </a>
-            </div>
-            <div class="login">
-                <a href="login.php" class="login-link"> Login </a>
-            </div>
-            <div class="perfil">
-                <a href="perfil.php"><img class="perfil-img" src="img/perfil.png"></a>
-            </div>
-            <div class="carrinho">
-                <a href="carrinho.php"><img class="carrinho-img" src="img/carrinho.png"></a>
-            </div>
-            <div class="ferramenta">
-                <a href="configura.php"><img class="ferramenta-img" src="img/ferramenta.png"></a>
+    <?php
+    include("navbar.php");
+    ?>
+
+    <div id="menu2">
+        <div id="pp">
+            <div></div>
+            <img src="./imgs/pequenoprincipe.jpg" alt="" style="float: left; margin-right: 30px">
+            <label for="" id="tpp">O pequeno príncipe</label>
+            <label for="" style="display:block;" id="app">Antoine de Saint-Exupéry</label>
+            <p style="display: block; width: 630px;">O Pequeno Príncipe é uma obra prima que conduz o leitor a uma viagem de descobertas pela essência humana.Este clássico
+                atemporal de Antoine de Saint-Exupéry narra a história sobre o encontro de um aviador e um menino com “cabelos da cor do
+                ouro”. À medida que o principezinho conta ao aviador sobre sua rotina no asteroide B 612, sua rosa única, o perigo dos
+                baobás, sua raposa e os moradores de outros planetas, um novo
+                olhar sobre a vida e o mundo é revelado.</p>
+        </div>
+
+        <div id="eh">
+            <div>
+                <img src="./imgs/evelynhugo.jpg" alt="" style="float: left; margin-right: 30px">
+                <label for="" id="teh">Os sete maridos de Evelyn Hugo</label>
+                <label for="" style="display:block;" id="aeh">Taylor Jenkins Reid</label>
+                <p style="display: block; width: 630px;">Evelyn Hugo, uma das maiores estrelas de Hollywood, agora a aproximar-se dos 80 anos, decide finalmente contar tudo
+                    sobre a sua vida recheada de glamour e de uma boa dose de escândalos. Quando escolhe a desconhecida Monique Grant para
+                    escrever a sua história, todos ficam surpreendidos, incluindo a própria jornalista.</p>
             </div>
 
         </div>
-        <div class="home">
-            <a href="index.php" class="home-link"> <img class="home-img" src="./img/home.png"></a>
-
-        </div>
-
     </div>
-   
+
+
 
 </body>
--- navbar.php
+++ navbar.php
@@ -3,16 +3,16 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" type="text/css" href="css/nav.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="./css/nav.css" media="screen" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 
 <body>
-
     <div class="div-logo">
-        <img class="logo" src="img/logo.png">
+
+        <img class="logo" src="./imgs/logo.png">
         <div class="tools">
             <div class="cadastro">
                 <a href="cadastro.php" class="cadastro-link"> Cadastro </a>
@@ -21,32 +21,27 @@
                 <a href="login.php" class="login-link"> Login </a>
             </div>
             <div class="perfil">
-                <a href="perfil.php"><img class="perfil-img" src="imgs/perfil.png"></a>
+                <a href="perfil.php"><img class="perfil-img" src="./imgs/perfil.png"></a>
             </div>
             <div class="carrinho">
-                <a href="carrinho.php"><img class="carrinho-img" src="imgs/carrinho.png"></a>
+                <a href="carrinho.php"><img class="carrinho-img" src="./imgs/carrinho.png"></a>
             </div>
             <div class="ferramenta">
-                <a href="configura.php"><img class="ferramenta-img" src="imgs/ferramenta.png"></a>
+                <a href="configura.php"><img class="ferramenta-img" src="./imgs/ferramenta.png"></a>
             </div>
 
         </div>
         <div class="home">
-           <a href="index.php" class="home-link"> <img class="home-img" src="imgs/home.png"></a>
-            <input type="text" class="textbox" placeholder="Digite algo..." list="sugestoes">
+           <a href="index.php" class="home-link"> <img class="home-img" src="./imgs/home.png"></a>
+            <input type="text" class="textbox" placeholder="Pesquisar" list="sugestoes">
             <datalist id="sugestoes">
                 <option value="O pequeno príncipe">
                 <option value="Os sete maridos de Evelyn Hugo">
-                <option value="Harry Potter e a Pedra Filosofal">
+                <option value="Percy Jackson e o Ladrão de Raios">
                 <!-- Adicione mais sugestões conforme necessário -->
             </datalist>
-            <a href="categorias.php" class="grupo-link"> Categorias</a>
-            <a href="bestsallers.php" class="grupo-link"> Bestsellers </a>
             <a href="autores.php" class="grupo-link"> Autores</a>
-            <a href="index.php" class="grupo-link"> Editoras</a>
-            <a href="index.php" class="grupo-link"> E-books</a>
-            <a href="index.php" class="grupo-link"> Sagas </a>
-            <a href="index.php" class="grupo-link"> Sagas </a>     
+            <a href="sagas.php" class="grupo-link">Sagas</a>     
              <a href="duvida.php"><img class="duvida-img" src="imgs/duvida.png"></a>
             
         </div>
